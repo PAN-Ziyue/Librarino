c443c7d38e4f0dfdfa44b113273eb4e881c03486
Raymond-Ziyue <3180105354@zju.edu.cn>
2020-04-05 20:52:51 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,36 +1,48 @@
 const electron = require('electron')
 const app = electron.app
 const BrowserWindow = electron.BrowserWindow
+const Menu = electron.Menu
 
-app.on('ready',()=>{
+const template = [{
+    label: 'System',
+    submenu: [
+        {
+            label: 'About',
+            role: 'about',
+            click: () => {
+                electron.shell.openExternal('https://github.com/PAN-Ziyue/Librarino/wiki/About-Librarino')
+            }
+        },
+        {
+            label: 'Log out',
+            role: 'reload'
+        },
+        { type: 'separator' },
+        {
+            label: 'Close',
+            role: 'quit'
+        }
+    ]
+}]
+
+app.on('ready', () => {
     mainWindow = new BrowserWindow({
         width: 800,
         minWidth: 680,
-        height:600,
-        webPreferences: {nodeIntegration: true}
+        height: 600,
+        webPreferences: { nodeIntegration: true }
     })
     mainWindow.loadFile('index.html')
-    mainWindow.on('closed', ()=>{
+    const menu = Menu.buildFromTemplate(template)
+    Menu.setApplicationMenu(menu)
+    mainWindow.on('closed', () => {
         mainWindow = null
     })
 })
 
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
-      app.quit()
+        app.quit()
     }
 })
 
-// const template=[{
-//     label: 'Edit',
-//     submenu:[
-//         {
-//             label: 'About',
-//             role: 'about',
-//             click: ()=>{
-//                 electron.shell.openExternal('https://')
-//             }
-//         },
-
-//     ]
-// }]
